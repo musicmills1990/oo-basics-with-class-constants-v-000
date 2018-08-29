3cc7c408686e71cdb2bcd371475ca1e686ba9a03
Marc Mills <mdmills1990@gmail.com>
2018-08-29 01:18:30 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,14 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title :genre
 
   GENRES = []
 
+
+  def genre=(genre)
+    @genre = GENRES
+  end
+  
   def initialize(title)
     @title = title
   end
