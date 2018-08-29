94a380a7d2532bb2df35f30fc26e60d0cbd68b59
Marc Mills <mdmills1990@gmail.com>
2018-08-29 01:27:10 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
+    @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << brand
+    end
+  end
+
+  def brand=(brand)
     @brand = brand
   end
 
